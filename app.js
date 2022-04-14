e4e2cc9eda9247986a0722a988dadcccd0bf2d96
kraskov <90540827+kraskov@users.noreply.github.com>
2022-04-14 17:52:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const T = require("tesseract.js")
+const T = require("tesseract.js") // constando o tesseract
 
 T.recognize('./imagem.png', 'eng', { logger: e => console.log(e) })
     .then(out => console.log(out))
+
+// console.log do resultado
